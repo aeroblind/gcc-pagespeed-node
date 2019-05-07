bb87725b9eb9804ada6c3adbe3d2e10e6787ba7f
Mark Eaton <marke@nobrainer2.com>
2019-05-07 08:25:11 -0500

    added back prod endpoints

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   websites: [
-    // {
-    //   id: 'bcom-prod',
-    //   url: 'https://www.blinds.com',
-    // },
+    {
+      id: 'bcom-prod',
+      url: 'https://www.blinds.com',
+    },
     {
       id: 'bcom-dev',
       url: 'https://dev.autobahn.blinds.com',
@@ -18,10 +18,10 @@ module.exports = {
       id: 'bcom-uat',
       url: 'http://uat.autobahn.blinds.com',
     },
-    // {
-    //   id: 'bcom-prod-plp',
-    //   url: 'https://www.blinds.com/c/faux-wood-blinds/40',
-    // },
+    {
+      id: 'bcom-prod-plp',
+      url: 'https://www.blinds.com/c/faux-wood-blinds/40',
+    },
     {
       id: 'bcom-dev-plp',
       url: 'https://dev.autobahn.blinds.com/c/faux-wood-blinds/40',
@@ -34,10 +34,10 @@ module.exports = {
       id: 'bcom-uat-plp',
       url: 'http://uat.autobahn.blinds.com/c/faux-wood-blinds/40',
     },
-    // {
-    //   id: 'bcom-prod-pip',
-    //   url: 'https://www.blinds.com/p/levolor-visions-2-faux-wood-blind/503426',
-    // },
+    {
+      id: 'bcom-prod-pip',
+      url: 'https://www.blinds.com/p/levolor-visions-2-faux-wood-blind/503426',
+    },
     {
       id: 'bcom-dev-pip',
       url: 'https://dev.autobahn.blinds.com/p/levolor-visions-2-faux-wood-blind/503426',
