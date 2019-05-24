6146031ed1cddc7b05dc169b950273bbb66e53c5
Mark Eaton <marke@nobrainer2.com>
2019-05-24 18:32:35 -0500

    minor updates

--- config.js
+++ config.js
@@ -24,35 +24,103 @@ module.exports = {
     },
     {
       id: 'bcom-prod-plp',
-      url: 'https://www.blinds.com/c/faux-wood-blinds/40',
+      url: 'https://www.blinds.com/c/faux-wood-blinds/54',
     },
     {
       id: 'bcom-dev-plp',
-      url: 'https://dev.autobahn.blinds.com/c/faux-wood-blinds/40',
+      url: 'https://dev.autobahn.blinds.com/c/faux-wood-blinds/54',
     },
     {
       id: 'bcom-stage-plp',
-      url: 'http://stage.autobahn.blinds.com/c/faux-wood-blinds/40',
+      url: 'http://stage.autobahn.blinds.com/c/faux-wood-blinds/54',
     },
     {
       id: 'bcom-uat-plp',
-      url: 'http://uat.autobahn.blinds.com/c/faux-wood-blinds/40',
+      url: 'http://uat.autobahn.blinds.com/c/faux-wood-blinds/54',
     },
     {
       id: 'bcom-prod-pip',
-      url: 'https://www.blinds.com/p/levolor-visions-2-faux-wood-blind/503426',
+      url: 'https://www.blinds.com/p/levolor-visions-2-faux-wood-blind/503439',
     },
     {
       id: 'bcom-dev-pip',
-      url: 'https://dev.autobahn.blinds.com/p/levolor-visions-2-faux-wood-blind/503426',
+      url: 'https://dev.autobahn.blinds.com/p/levolor-visions-2-faux-wood-blind/503439',
     },
     {
       id: 'bcom-stage-pip',
-      url: 'http://stage.autobahn.blinds.com/p/levolor-visions-2-faux-wood-blind/503426',
+      url: 'http://stage.autobahn.blinds.com/p/levolor-visions-2-faux-wood-blind/503439',
     },
     {
       id: 'bcom-uat-pip',
-      url: 'http://uat.autobahn.blinds.com/p/levolor-visions-2-faux-wood-blind/503426',
+      url: 'http://uat.autobahn.blinds.com/p/levolor-visions-2-faux-wood-blind/503439',
+    },
+    {
+      id: 'lowes-home',
+      url: 'https://blinds.lowes.com/',
+    },
+    {
+      id: 'lowes-plp',
+      url: 'https://blinds.lowes.com/c/Custom-Wood-Blinds?limited=0&page=1&pageSize=18&sortCode=-Most%20Popular',
+    },
+    {
+      id: 'lowes-pip',
+      url: 'https://blinds.lowes.com/product/detail.action?sku=Levolor-Real-Wood-Blinds&groupName=Wood-Blinds&xdata-width=24&xdata-height=36,
+    },
+    {
+      id: 'select-home',
+      url: 'https://www.selectblinds.com',
+    },
+    {
+      id: 'select-plp',
+      url: 'https://www.selectblinds.com/wood-blinds.html',
+    },
+    {
+      id: 'select-pip',
+      url: 'https://www.selectblinds.com/woodblinds/2-inch-premier-wood-blind.html,
+    },
+    {
+      id: 'bb-home',
+      url: 'https://budgetblinds.com',
+    },
+    {
+      id: 'bb-plp',
+      url: 'https://budgetblinds.com/our-products/blinds/wood-blinds/',
+    },
+    {
+      id: 'bg-home',
+      url: 'https://www.blindsgalore.com',
+    },
+    {
+      id: 'bg-plp',
+      url: 'https://www.blindsgalore.com/wood-blinds',
+    },
+    {
+      id: 'bg-pip',
+      url: 'https://www.blindsgalore.com/product/702199/blindsgalore-hardwood-blinds-slats'
+    },
+    {
+      id: 'steves-home',
+      url: 'https://www.stevesblindsandwallpaper.com'
+    },
+    {
+      id: 'steves-plp',
+      url: 'https://www.stevesblindsandwallpaper.com/blinds/search/?brand=steves-exclusive-collection&product_type=wood-blinds'
+    },
+    {
+      id: 'steves-pip',
+      url: 'https://www.stevesblindsandwallpaper.com/blinds/product/6952-steves-good-2-wood-blind'
+    },
+    {
+      id: 'blindster-home',
+      url: 'https://www.blindster.com'
+    },
+    {
+      id: 'blindster-plp',
+      url: 'https://www.blindster.com/wood-blinds/'
+    },
+    {
+      id: 'blindster-pip',
+      url: 'https://www.blindster.com/wood-blinds/2-inch-wood-blinds/'
     },
   ],
   pageSpeedBaseUrl: 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed'
