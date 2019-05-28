29d3ef72ea9db36c1c77c3d93ed3d25f0403058c
Mark Eaton <marke@nobrainer2.com>
2019-05-28 15:14:29 -0500

    error handling

--- util/helpers.js
+++ util/helpers.js
@@ -1,10 +1,17 @@
 
 function parsePageSpeedResults(res) {
     const { audits, categories } = res.data.lighthouseResult;
+    let metrics = null;
+    try {
+      metrics = audits.metrics.details.items[0]
+    } catch (err) {
+      console.log("Metrics not found");
+      console.error(err);
+    }
     return {
       fetchTime: res.data.lighthouseResult.fetchTime,
       score: categories.performance.score,
-      metrics: audits.metrics.details.items[0],
+      metrics: metrics,
     }
 }
 
